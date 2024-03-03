587086e733f779ca4ed4b0436f1b73e64ba72915
Planet-Seven <92541389+Planet-Seven@users.noreply.github.com>
2024-03-03 15:04:54 +0100

    Add files via upload

---
+++ Arduino/fourrier.ino
@@ -0,0 +1,26 @@
+#include "SevSeg.h"
+SevSeg sevseg;
+
+float frequency = 0; 
+
+void setup() {
+  byte numDigits = 4;
+  byte digitPins[] = {10, 11, 12, 13};
+  byte segmentPins[] = {9, 2, 3, 5, 6, 8, 7, 4};
+  bool resistorsOnSegments = true;
+  byte hardwareConfig = COMMON_CATHODE;
+
+  Serial.begin(9600);
+
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+}
+void loop() {
+  if(Serial.available())
+  {
+    frequency = Serial.read();
+    frequency = (frequency / 255 )* 4400;
+  }
+
+  sevseg.setNumber( floor(frequency) );
+  sevseg.refreshDisplay();
+}
